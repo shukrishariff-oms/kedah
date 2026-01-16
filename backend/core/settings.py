fb2f7a26a19892d905a0513bb45e16a08b612b57
Shukri Shariff <shukrishariff88@gmail.com>
2026-01-16 23:00:10 +0800

    git commit -m "fix: Add frontend domain to CORS origins"

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -164,6 +164,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://kedah.ohmaishoot.com",
+    "https://api.ohmaishoot.com",
 ]
 
 FRONTEND_ORIGIN = os.getenv('FRONTEND_ORIGIN')
