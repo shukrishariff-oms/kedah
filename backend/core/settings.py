820c12f6d04b851a8061bd2fe7cd3cc0badd12c5
Shukri Shariff <shukrishariff88@gmail.com>
2026-01-16 23:02:54 +0800

    fix: Allow all CORS origins (debug)

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -160,7 +160,7 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
