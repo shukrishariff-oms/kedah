8957e151b658997db35b5b3f013e3508651a5aac
Shukri Shariff <shukrishariff88@gmail.com>
2026-01-17 16:08:57 +0800

    Fix: Export getDistricts from places API

--- frontend/src/api/places.js
+++ frontend/src/api/places.js
@@ -2,3 +2,4 @@ import client from './client';
 
 export const getPlaces = (params) => client.get('/places/', { params });
 export const getPlace = (slug) => client.get(`/places/${slug}/`);
+export const getDistricts = () => client.get('/districts/');
